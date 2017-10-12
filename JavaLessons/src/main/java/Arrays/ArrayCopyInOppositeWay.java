3dea6446ed299e29721ce0f9724c4e150377b8a2
TetianaKryvets <tetiana.kryvets@itera.no>
2017-10-12 10:07:22 +0300

    array in the opposit way works correctly

--- JavaLessons/src/main/java/Arrays/ArrayCopyInOppositeWay.java
+++ JavaLessons/src/main/java/Arrays/ArrayCopyInOppositeWay.java
@@ -1,9 +1,5 @@
 package Arrays;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
 /**
  * User: tetiana.kryvets
  * Date: 7/30/17
@@ -14,55 +10,43 @@ public class ArrayCopyInOppositeWay {
         int size=5;
         int[][] array2dim = new int[size][size];
 
+        fillArray(size, array2dim);
+        System.out.println("-------");
+
+        printArray(size, array2dim);
+        System.out.println("-------");
+
+        changeElementsByDiagonal(size, array2dim);
+        printArray(size, array2dim);
+
+    }
+
+    private static void fillArray(int size, int[][] array2dim) {
         for (int i=0; i<=size-1; i++){
             for (int j=0; j<=size-1; j++) {
                 array2dim[i][j] = i+1;
             }
         }
+    }
 
+    private static void changeElementsByDiagonal(int size, int[][] array2dim) {
+        int tmp = 0;
         for (int i=0; i<=size-1; i++){
-            for (int j=0; j<=size-1; j++) {
-                System.out.print(array2dim[i][j]);
-            }
-            System.out.println();
-            System.out.println("---------------------------------------------");
-        }
-
-        //Надо отобразить по диагонали
-        //todo: напутала с индексами
-        int tmp =0;
-        for (int i=0; i<=size-1; i++){
-            for (int j=0; j<=size-1; j++) {
-                //if (array2dim[m][n]!=array2dim[n][m]) {
+            for (int j=i+1; j<=size-1; j++) {
                 tmp = array2dim[i][j];
                 array2dim[i][j] = array2dim[j][i];
                 array2dim[j][i] = tmp;
+            }
+        }
+    }
+
+    private static void printArray(int size, int[][] array2dim) {
+        for (int i=0; i<=size-1; i++){
+            for (int j=0; j<=size-1; j++) {
 
                 System.out.print(array2dim[i][j]);
-               // }
             }
             System.out.println();
         }
-
-//        for (int i=0; i<=size-1; i++){
-//            for (int j=0; j<=size-1; j++) {
-//                System.out.print(array2dim[i][j]);
-//            }
-//            System.out.println();
-//        }
-
-        //todo: вывводит исходную матрицу
-        System.out.println("---------------------------------------------");
-        System.out.println(Arrays.deepToString(array2dim));
-
-
-
-//        //Enter n value manually:
-//        System.out.println("Enter a value:");
-//        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
-//        String numa = reader.readLine();
-//        int n = Integer.parseInt(numa);
-
-
     }
 }
