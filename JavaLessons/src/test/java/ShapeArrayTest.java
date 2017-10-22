cbc769bd82da7b8c7cf50e8d790b72088afc4eba
TetianaKryvets <tetiana.kryvets@itera.no>
2017-10-22 17:01:32 +0300

    JUnit test for Spahes

--- JavaLessons/src/test/java/ShapeArrayTest.java
+++ JavaLessons/src/test/java/ShapeArrayTest.java
@@ -13,9 +13,9 @@ public class ShapeArrayTest {
 
     @Test
     public void testAreaIsCorrect() {
-        assertEquals("Area of triangle is incorrect", (new Triangle(3,4,5,"Triangle")).shapeArea(), 6.0);
-        assertEquals("Area of triangle is incorrect", (new Triangle(3,5,"Triangle")).shapeArea(), 7.5);
-        assertEquals("Area of triangle is incorrect", (new Rectangle(2,-5,"Rectangle")).shapeArea(), 10.0);
+        assertEquals("Area of triangle is incorrect", 6.0, (new Triangle(3,4,5,"Triangle")).shapeArea());
+        assertEquals("Area of triangle is incorrect", 7.5, (new Triangle(3,5,"Triangle")).shapeArea());
+        assertEquals("Area of triangle is incorrect", 10.0, (new Rectangle(2,-5,"Rectangle")).shapeArea());
     }
 
 
