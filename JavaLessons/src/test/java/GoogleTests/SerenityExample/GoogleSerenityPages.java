57814a9ef598d8c2d78fafb74b72001b30e31912
TetianaKryvets <tetiana.kryvets@itera.no>
2017-12-11 18:00:33 +0200

    example with Serenity

--- JavaLessons/src/test/java/GoogleTests/SerenityExample/GoogleSerenityPages.java
+++ JavaLessons/src/test/java/GoogleTests/SerenityExample/GoogleSerenityPages.java
@@ -1,45 +1,50 @@
 package GoogleTests.SerenityExample;
 
 import GoogleTests.WebDriverExample.BaseTests;
+import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * User: tetiana.kryvets
  * Date: 12/4/2017
  */
-public class GoogleSerenityPages {
+@DefaultUrl("http://www.google.com")
+public class GoogleSerenityPages extends PageObject {
 
-    @FindBy(id = "sfdiv")
+//    @Managed
+//    WebDriver driver;
+
+//    @Managed(driver = "chrome")
+
+    String first_link;
+
+    @FindBy(id = "lst-ib")
     WebElementFacade searchField;
 
-    public void findElementByName(String entityName) {
+    @FindBy(xpath = "(//div/h3/a)[1]")
+    WebElementFacade resultedLink;
 
-        searchField.then().click();
-
-        By searchLocator = By.id("sfdiv");
-        WebElement searchField = BaseTests.driver.findElement(searchLocator);
-
-        Actions actions = new Actions(BaseTests.driver);
-        actions.moveToElement(searchField);
-        actions.click();
-        actions.sendKeys(entityName);
-        actions.build().perform();
-        actions.sendKeys(Keys.ENTER);
-        actions.build().perform();
-        //searchField.sendKeys();
-
-        WebElement pictureButton;
-        pictureButton = (new WebDriverWait(BaseTests.driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div/a[text()='Зображення']")));
-        pictureButton.click();
+    public void searchFor(String entityName) {
+        getDriver().findElement(By.id("lst-ib"));
+        searchField.typeAndEnter(entityName);
     }
+
+    public String selectTheFirstLink() {
+        first_link = resultedLink.getAttribute("href");
+        resultedLink.click();
+        return first_link;
+    }
+
+
+
+
 
     public String clickOnTheFirstPicture() {
         WebElement firstPictureButton = BaseTests.driver.findElement(By.xpath("(//div[@class='rg_bx rg_di rg_el " +
--- JavaLessons/src/test/java/GoogleTests/SerenityExample/GoogleSerenityTest.java
+++ JavaLessons/src/test/java/GoogleTests/SerenityExample/GoogleSerenityTest.java
@@ -5,26 +5,38 @@ package GoogleTests.SerenityExample;
  * Date: 12/4/2017
  */
 import GoogleTests.WebDriverExample.BaseTests;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Title;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.github.webdriverextensions.WebDriverExtensionsContext.getDriver;
 import static org.junit.Assert.assertEquals;
 
-public class GoogleSerenityTest extends BaseTests {
-    GoogleSerenityPages serenityPages = new GoogleSerenityPages();
-    String className, linkToPicture;
+@RunWith(SerenityRunner.class)
+public class GoogleSerenityTest {
+    
+    @Managed
+    WebDriver driver;
+
+    String className, firstLink;
+
+
+    @Steps
+    GoogleSerenitySteps serenity_steps;
 
     @Test
-    public void openFirstPictureTest() {
-//        logger.
-//        pages.findElementByName("suzuki sx4");
-//        linkToPicture = pages.clickOnTheFirstPicture();
-//        pictureShouldBeOpened();
-//        className = pages.openInNewWindow();
-//        pages.switchToNewWindow();
+    @Title("Open the first founded link and check that link is correct")
+    public void openFirstLinkTest() {
+        serenity_steps.search_by_value("qa fest");
+        firstLink = serenity_steps.find_the_first_link();
+        serenity_steps.link_should_be_opened();
 
     }
 
-    public void pictureShouldBeOpened() {
-        assertEquals("suzuki sx4 - Пошук Google", BaseTests.driver.getTitle());
-    }
+
 }
